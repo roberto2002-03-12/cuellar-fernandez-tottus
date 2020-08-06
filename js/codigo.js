7424d1611bd4e37e56db54fc22f6dbe7e933f652
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2020-08-06 18:47:33 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,5 @@
+// una venta emergenete---------------------------// comtentarios de una linea//
+/* comentarios mutltilineea*/
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
